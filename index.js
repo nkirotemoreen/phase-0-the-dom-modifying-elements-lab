3af63cb9d10f01082619e6741682fecd1ea48048
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-29 12:37:46 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//remove the node child
+document.querySelector('main#main').remove();
+//createnew element
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML ="Moreen is the champion!"
